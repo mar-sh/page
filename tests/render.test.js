03ee24c7997ac066f0716734cfe078417a5cfda9
silversea-mar <marasi.samosir@silversea-media.id>
2019-12-30 15:11:38 +0700

    Add layout

--- tests/render.test.js
+++ tests/render.test.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-const html = fs.readFileSync(path.resolve(__dirname, "../public/index.html"), "utf8");
+const html = fs.readFileSync(
+	path.resolve(__dirname, "../public/index.html"),
+	"utf8"
+);
 require("@testing-library/jest-dom/extend-expect");
 
 jest.dontMock("fs");
@@ -14,7 +17,10 @@ describe("render page", function() {
 		jest.resetModules();
 	});
 
-	it("page exists", function() {
+	it("all elements exist", function() {
 		expect(document.getElementById("main")).toBeInTheDocument();
+		expect(document.getElementById("title")).toBeInTheDocument();
+		expect(document.getElementById("content")).toBeInTheDocument();
+		expect(document.getElementById("social")).toBeInTheDocument();
 	});
 });
